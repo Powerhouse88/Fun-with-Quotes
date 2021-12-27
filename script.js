d33521cbdf2e6dc08093b2ca5446377fa0962d26
Himanshu Goyal <90150092+Powerhouse88@users.noreply.github.com>
2021-12-27 16:47:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+const quoteContainer= document.getElementById('quote-container');
+const quoteText= document.getElementById('quote');
+const authorText= document.getElementById('author');
+const twitterBtn= document.getElementById('twitter');
+const newQuoteBtn= document.getElementById('new-quote');
+const loader= document.getElementById('loader');
+
+
+
+let apiQuotes=[];
+
+//LOADIMG FUNCTION
+function loading(){
+    loader.hidden=false;
+    quoteContainer.hidden=true;
+}
+
+// HIDE LOADING
+function complete(){
+    quoteContainer.hidden=false;
+    loader.hidden=true;
+}
+
+
+
+// SHOW NEW QUOTE
+function newQuote(){
+    loading();
+    // TO PICK RANDOM QUOTE FROM APIQUOTES ARRAY
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // check author field is blank and replace with UNKNKOWN
+    if(!quote.author) {
+        authorText.textContent='Unknown';
+    }
+    else{
+        authorText.textContent=quote.author;
+    }
+    // Cgheck quote lenght to determine STYLING
+    if(quote.text.length>120){
+        quoteText.classList.add('long-quote');
+    }
+    else{
+        quoteText.classList.remove('long-quote');
+    }
+    //SET OUOTE, HIDE LOADER
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+
+// QUOTES FROM API
+async function getQuotes() {
+ loading();
+ const apiUrl= 'https://type.fit/api/quotes';
+ try {
+   const response= await fetch(apiUrl);
+   apiQuotes = await response.json();
+   newQuote();
+ }catch(error){
+     //CATCH ERRORS HERE
+  }
+}
+
+// TWEET A QUOTE
+function tweetQuote(){
+    const twitterUrl= `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank'); 
+}
+
+// EVENT LISTENERS
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
+// ON LOADS
+getQuotes();
+
